5ff6155e91079050e848dbb4271729e7cb8b0447
AdrienAtallah <adrienatallah@gmail.com>
2016-12-04 18:54:22 -0500

    implement link adding

--- app.js
+++ app.js
@@ -14,8 +14,15 @@ app.controller('MainCtrl', [
 
         $scope.addPost = function(){
             if(!$scope.title || $scope.title === '') { return; }
-            $scope.posts.push({title: $scope.title, upvotes: 0});
+
+            $scope.posts.push({
+                title: $scope.title,
+                upvotes: 0,
+                link: $scope.link
+            });
+
             $scope.title = '';
+            $scope.link= '';
         };
 
         $scope.incrementUpvotes = function(post) {
