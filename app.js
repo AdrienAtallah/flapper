cbcc81ce697c5862440b8fc8323ead3604a41b4c
AdrienAtallah <adrienatallah@gmail.com>
2016-12-04 19:38:56 -0500

    add posts state

--- app.js
+++ app.js
@@ -54,6 +54,12 @@ app.controller('MainCtrl', [
                 templateUrl: '/home.html',
                 controller: 'MainCtrl'
             });
+            
+            .state('posts', {
+                url: '/posts/{id}',
+                templateUrl: '/posts.html',
+                controller: 'PostsCtrl'
+            });
 
             $urlRouterProvider.otherwise('home');
         }]);
