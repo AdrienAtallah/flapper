65d441ac3c8b98cb4b6bf65c34c56d73a6bde125
AdrienAtallah <adrienatallah@gmail.com>
2016-12-04 19:41:46 -0500

    add posts controller

--- app.js
+++ app.js
@@ -5,15 +5,7 @@ app.controller('MainCtrl', [
     'posts',
 
     function($scope, posts){
-
-        $scope.posts = [
-            {title: 'post 1', upvotes: 5},
-            {title: 'post 2', upvotes: 2},
-            {title: 'post 3', upvotes: 15},
-            {title: 'post 4', upvotes: 9},
-            {title: 'post 5', upvotes: 4}
-        ];
-
+            
         $scope.posts = posts.posts;
 
         $scope.addPost = function(){
@@ -35,31 +27,39 @@ app.controller('MainCtrl', [
 
     }]);
 
-    app.factory('posts', [function(){
-        var o = {
-            posts: []
-        };
+    app.controller('PostsCtrl', [
+        '$scope',
+        '$stateParams',
+        'posts',
+        function($scope, $stateParams, posts){
 
-        return o;
-    }]);
-
-    app.config([
-        '$stateProvider',
-        '$urlRouterProvider',
-        function($stateProvider, $urlRouterProvider) {
-
-            $stateProvider
-            .state('home', {
-                url: '/home',
-                templateUrl: '/home.html',
-                controller: 'MainCtrl'
-            });
-            
-            .state('posts', {
-                url: '/posts/{id}',
-                templateUrl: '/posts.html',
-                controller: 'PostsCtrl'
-            });
-
-            $urlRouterProvider.otherwise('home');
         }]);
+
+        app.factory('posts', [function(){
+            var o = {
+                posts: []
+            };
+
+            return o;
+        }]);
+
+        app.config([
+            '$stateProvider',
+            '$urlRouterProvider',
+            function($stateProvider, $urlRouterProvider) {
+
+                $stateProvider
+                .state('home', {
+                    url: '/home',
+                    templateUrl: '/home.html',
+                    controller: 'MainCtrl'
+                });
+
+                .state('posts', {
+                    url: '/posts/{id}',
+                    templateUrl: '/posts.html',
+                    controller: 'PostsCtrl'
+                });
+
+                $urlRouterProvider.otherwise('home');
+            }]);
