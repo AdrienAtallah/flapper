6f378e769f69435e40bc7cefedb9839846d6102c
AdrienAtallah <adrienatallah@gmail.com>
2016-12-10 20:20:33 -0500

    add get all posts route

--- flapper-news/routes/index.js
+++ flapper-news/routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+var mongoose = require('mongoose');
+var Post = mongoose.model('Post');
+var Comment = mongoose.model('Comment');
+
+router.get('/posts', function(req, res, next) {
+  Post.find(function(err, posts){
+    if(err){ return next(err); }
+
+    res.json(posts);
+  });
 
 module.exports = router;
